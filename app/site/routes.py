a45d4d4a74f6ed24d9516716fe7a459a51c40c4f
terrotar <pedro_guedes93@hotmail.com>
2021-08-15 17:38:00 -0300

    Few details of home_page.html was changed. Site Blueprint /getdata/city was implemented. Still need to develop the /getdata/lng/lat.

--- app/site/routes.py
+++ app/site/routes.py
@@ -8,11 +8,16 @@ from haversine import Unit
 site = Blueprint('site', __name__)
 
 
+# Route of home page with instructions of how to consume the API
+# and anothers functionalities.
 @site.route('/', methods=['GET'])
 def index():
     return render_template('home_page.html')
 
 
+# Route to calculate the distance based on longitude and latitude
+# inserted as input and submmited with buttons by the user in a more
+# visual display as alternative of the route API.
 @site.route("/distance", methods=["POST"])
 def distance():
     if(request.method == 'POST'):
@@ -32,3 +37,19 @@ def distance():
                                    address=address,
                                    kilometers=km,
                                    miles=miles)
+
+
+# Route to display data of an address based on longitude and latitude or
+# city and street name inserted as input and submmited with buttons by the user
+# in a more visual display as alternative of the route API. Does the same thing
+# of the Blueprint API /api/getdata but a little more friendly to understand
+# how does it work.
+@site.route("/getdata/city", methods=["POST"])
+def getdata_city():
+    if(request.method == 'POST'):
+        address = request.form["city"]
+        if(address):
+            data = requests.get(
+                f"https://geocode-maps.yandex.ru/1.x/?apikey=085b7527-0c65-43e9-b0e7-86b52fb93ffe&geocode={address}&format=json&lang=en-US&results=1")
+        return data.json()
+
