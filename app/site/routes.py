e7017a233d9003298e473ae04d58409b177c0c70
terrotar <pedro_guedes93@hotmail.com>
2021-08-15 17:38:36 -0300

    Formatted sites.routes due to Pep-8.

--- app/site/routes.py
+++ app/site/routes.py
@@ -52,4 +52,3 @@ def getdata_city():
             data = requests.get(
                 f"https://geocode-maps.yandex.ru/1.x/?apikey=085b7527-0c65-43e9-b0e7-86b52fb93ffe&geocode={address}&format=json&lang=en-US&results=1")
         return data.json()
-
