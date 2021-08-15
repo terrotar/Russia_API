55ab8f39c7bd8b2606ab79e1adc78e7aee18ef00
terrotar <pedro_guedes93@hotmail.com>
2021-08-15 06:17:42 -0300

    Routes of API are done. Need to implement the site now.

--- README.md
+++ README.md
@@ -35,9 +35,7 @@ Then you just have to open a terminal inside the directory and type:
 
 *** About the project ***
 
-    If the specified address is located inside the MKAD, the distance does not need to be calculated. I didn't know how to
-specify MKAD in geolocation because it's not a point, like an address but a huge ring road, so to solve that barrier
-I consider the distance between Moscow(been the center of a circular form) with MKAD, and calculate the area of that distance
-in km using one library(haversine) found in Pipfile. Basically, haversine will calculate distance between 2 points with the
-parameter you desire, such as miles, kms, meters. The result was 16.2 kms rounded, so from Moscow until 16.2 kms the program
+If the specified address is located inside the MKAD, the distance does not need to be calculated.To calculate the distance
+was used the library haversine found in Pipfile. Basically, haversine will calculate distance between 2 points with the
+parameter you desire, such as miles, kms, meters. The result was 12.9 kms rounded, so from Moscow until 12.9 kms(MKAD) the program
 won't calculate in due to consider inside MKAD.
--- app/api/routes.py
+++ app/api/routes.py
@@ -1,11 +1,36 @@
 from flask import Blueprint, request
 import requests
 
+import haversine as hs
+from haversine import Unit
 
+
+# instance of Blueprint api
 api = Blueprint('api', __name__, url_prefix='/api')
 
 
-@api.route("/<address>", methods=["GET"])
+# Route that calculate the distance between MKAD and the
+# address typed by the user. It must be coordinate(first lng
+# then lat) and float.
+@api.route("/distance/<float:lng>/<float:lat>", methods=["GET"])
+def distance(lng, lat):
+    if(request.method == 'GET'):
+        moscow = (37.622513, 55.75322)
+        address = (lng, lat)
+        dist = round(hs.haversine(moscow, address), 1)
+        if(dist <= 12.9):
+            return {'ValueError': 'Distance inside MKAD.'}
+        else:
+            miles = round(hs.haversine(moscow, address, unit=Unit.MILES), 1)
+            return {'distance': {
+                                'Kilometers': dist,
+                                'Miles': miles}
+                    }
+
+
+# Route to get data of a certain place. It can be a name(Moscow) or
+# coordinates(37.622513 55.75322, 37.622513,55.75322).
+@api.route("/getdata/<address>", methods=["GET"])
 def getdata(address):
     if(request.method == 'GET'):
         data = requests.get(
